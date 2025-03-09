87e1bdc83b56c38fdd9a1f8a998e4d0d6ed254c9
Alexander Fedorenko <alfedmix@gmail.com>
2025-03-09 01:41:06 +0100

    Add formatDate and formatTime locale specific functions.

--- packages/lib/src/components/LocaleContext/LocaleContext.tsx
+++ packages/lib/src/components/LocaleContext/LocaleContext.tsx
@@ -17,9 +17,36 @@ export interface LocaleContextProps {
 export const LocaleContextProvider = ({children}: LocaleContextProps) => 
   <LocaleContext.Provider value={new LocaleContextType()} >{children}</LocaleContext.Provider>;
 
+export function formatDate(d?: Date, locale?: string): string {
+  return (d || new Date()).toLocaleDateString(locale);
+}
+
+export function combineDateAndTime(d?: Date, seconds?: number): Date {
+  const datePart = d || new Date();
+  const result = new Date(datePart.getFullYear(), datePart.getMonth(), datePart.getDate());
+  result.setSeconds(seconds ?? 0);
+  return result;
+}
+
+export function formatTime(seconds?: number, 
+  skipSeconds: boolean = false, locale?: string, timeZone?: string): string {
+  const d = combineDateAndTime(undefined, seconds);
+  const formattedTime = d.toLocaleTimeString(locale, {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: skipSeconds ? undefined : 'numeric',
+    timeZone
+  });
+  return formattedTime;
+};
+
 export const useLocale = () => {
   const locale = useContext(LocaleContext);
-  return { locale };
+  return { locale, 
+    formatTime: (seconds?: number, skipSeconds: boolean = false) => 
+      (formatTime(seconds, skipSeconds, locale.locale, locale.timeZone)),
+    formatDate: (d?: Date) => formatDate(d, locale.locale)
+  };
 }
 
 export function getCurrentLocale() {
